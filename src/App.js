9d6bafc2f66934dac6e699095bd497730505b02c
Ajerrar <sofianeajerrar@hotmail.com>
2020-02-17 15:46:23 +0100

    Icone finalisé avec le hover

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import Navbar from './jsx/Navbar';
 
 function App() {
   return (
     <div>
-      
+      <Navbar/>
     </div>
   );
 }
---
+++ src/jsx/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../sass/style.sass';
+
+let Navbar = () => {
+    return (
+        <nav className='navbar-brand'>
+            <ul className='nav flex-column container'>
+                <li className='nav-item d-flex justify-content-center'>
+                    <a href="#" className='nav-link active hoverNav greenColor'>
+                        <i className="fas fa-meh-blank fa-2x hoverIcon"></i>
+                    </a>
+                </li>
+                <li className='nav-item d-flex justify-content-center'>
+                    <a href="#" className='nav-link active hoverNav greenColor'>
+                        <i className="fab fa-html5 fa-2x hoverIcon"></i>
+                    </a>
+                </li>
+                <li className='nav-item d-flex justify-content-center'>
+                    <a href="#" className='nav-link active hoverNav greenColor'>
+                        <i className="fas fa-laptop-code fa-2x hoverIcon"></i>
+                    </a>
+                </li>
+                <li className='nav-item d-flex justify-content-center'>
+                    <a href="#" className='nav-link active hoverNav greenColor'>
+                        <i className="fas fa-mobile fa-2x hoverIcon"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
